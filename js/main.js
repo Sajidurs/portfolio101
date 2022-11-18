9ca4173b5e2c0ec7bd15871f1583f30d3287c3ab
Raisel <raieslahmed985@gmail.com>
2022-11-18 22:47:59 +0600

    Latenight

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// $(selector).countMe(delay,speed)
+$("#counter").countMe(40,65);
+
+// Testimonials
+$(document).ready(function () {
+    $('.responsive-testimonals').responsiveTestimonals();
+  });
